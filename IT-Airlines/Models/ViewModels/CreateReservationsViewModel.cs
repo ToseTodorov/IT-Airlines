e9743243648c5575a5b0bfac9b4c97a34c655330
Tose Todorov <tose.todorov@hotmail.com>
2019-10-09 17:13:23 +0200

    Small changes

---
+++ IT-Airlines/Models/ViewModels/CreateReservationsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using IT_Airlines.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IT_Airlines.Models.ViewModels
+{
+    public class CreateReservationsViewModel
+    {
+        BeginReservationsViewModel PartialReservation { get; set; }
+
+        Flight Flight { get; set; }
+        Flight ReturnFlight { get; set; }
+        List<Reservation> Reservations { get; set; }
+    }
+}
