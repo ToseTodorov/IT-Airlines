93640b4be4301558e551b9e24d6789dcbb53223a
Venko Stojanov <venkostojanov@hotmail.com>
2019-12-08 17:46:07 +0100

    Added UserRoles model

---
+++ IT-Airlines/Models/UserRoles/AddToRoleModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IT_Airlines.Models.UserRoles
+{
+    public class AddToRoleModel
+    {
+        public string Email { get; set; }
+        public string selectedRole { get; set; }
+        public List<String> roles { get; set; }
+
+        public AddToRoleModel()
+        {
+            roles = new List<string>();
+        }
+    }
+}
