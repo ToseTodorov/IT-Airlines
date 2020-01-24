0b8bf3aa886a99f7d5f52b8f5cf64ec6bf61cc4e
Kiril_Ristov <ristov.kiril@yahoo.com>
2020-01-24 20:28:30 +0100

    datepicker on create reservation

--- IT-Airlines/App_Start/BundleConfig.cs
+++ IT-Airlines/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace IT_Airlines
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,15 +22,15 @@ namespace IT_Airlines
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/jquery-ui.js",
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/bootbox.js"
                       ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/jquery-ui.css",
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/jquery-ui.css"
+));
         }
     }
 }
